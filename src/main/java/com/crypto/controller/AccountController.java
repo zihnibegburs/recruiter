29a9c4fe63d61b91f8831aad6935cfd617eea496
begburs <zihnibegburs@getbaraka.com>
2024-05-18 15:36:33 +0300

    Added my project

---
+++ src/main/java/com/crypto/controller/AccountController.java
@@ -0,0 +1,31 @@
+package com.crypto.controller;
+
+import com.crypto.persistance.entity.Jobs;
+import com.crypto.service.JobScratcherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController("/recruiter")
+public class AccountController {
+
+    @Autowired
+    JobScratcherService jobScratcherService;
+
+    @GetMapping("/scratch")
+    public void scratchJobs() {
+        jobScratcherService.scratch();
+    }
+
+    @GetMapping("/jobs")
+    public List<Jobs> getJobs() {
+
+        Jobs test = new Jobs();
+        test.setTitle("title");
+        test.setUrl("localhost:3030");
+        test.setDescription("description");
+        return List.of(test);
+    }
+
+}
